ece04f3cf5f2fec10ec7bf1f82a7789a50843ac0
teschnm1 <magnus.teschner@roche.com>
2021-02-15 12:04:28 +0100

    Saubere Kommentare, Singleplayer Paddle verbessert

--- KLassenlocal.py
+++ KLassenlocal.py
@@ -1,7 +1,10 @@
 import pygame
 import time
 import random
+
+#Farben
 light_grey = (200,200,200)
+
 class Ball:
     def __init__(self, screen, color, posX, posY, radius):
         self.screen = screen
@@ -16,31 +19,38 @@ class Ball:
         self.counter = 0
         self.draw()
 
+    #Kreis zeichnen
     def draw(self):
         pygame.draw.circle(self.screen, self.color, (self.posX, self.posY), self.radius)
 
+    #Start Moving in zufällige Richtung
     def start_moving(self):
         a = random.randint(0,1)
         liste = [0.3, -0.3]
         self.dx = liste[a]
-        x = 0
-        while  -0.1 < x < 0.1:
-            x = random.uniform(-0.33, 0.33)
-        self.dy = x
+        y = 0
+        while  -0.1 < y < 0.1:
+            y = random.uniform(-0.33, 0.33)
+        self.dy = y
 
+    #Moving mit Richtung dx, dy
     def move(self):
         self.posX += self.dx
         self.posY += self.dy
 
+    #Wenn auf wall trifft reverse dy
     def wall_collision(self):
         self.dy = -self.dy
 
+    #Paddle Collision
     def paddle_collision(self, counter, counter1, paddle):
+        #Wenn erster Kontakt dx hoch
         if self.dx > 0 and counter == 0:
             self.dx = 0.6
         elif self.dx < 0 and counter == 0:
             self.dx = -0.6
         self.dx = -self.dx
+        #Nach jedem Paddle Kontakt wird Ball, Paddle schneller
         if self.dx > 0:
             self.dx += 0.01
         elif self.dx < 0:
@@ -48,9 +58,9 @@ class Ball:
         if counter1 != 0:
             paddle.dy += 0.02
 
-
+    #Restart Position und dx, dy
     def restart_pos(self):
-        if self.posX <=4 or self.posX >= 895:
+        if self.posX <= 4 or self.posX >= 895:
             self.posX = 450
             self.posY = 250
             self.dx = 0
@@ -71,45 +81,55 @@ class Paddle:
         self.dy = 0
         self.draw()
 
+    #Rechteck zeichnen
     def draw(self):
         pygame.draw.rect(self.screen, self.color, (self.posX, self.posY, self.width, self.height) )
 
+    #Nachverfolgung Ball, Singleplayer
     def move_on(self, ball):
-        if ball.posY > self.posY - 2 and ball.posX < 450:
-            self.posY += 0.6
+        if ball.posY > self.posY - 10 and ball.posX < 500:
+            self.posY += 0.45 + self.dy
 
-        if ball.posY < self.posY + 2 and ball.posX < 450:
-            self.posY -= 0.6
+        if ball.posY < self.posY + 10 and ball.posX < 500:
+            self.posY -= 0.45 + self.dy
 
+    #Moven auf Pfeiltasten
     def move(self):
         if self.state == "up":
             self.posY -= 0.45 + self.dy
         elif self.state == "down":
             self.posY += 0.45 + self.dy
 
-        #print(self.dy)
+    #Nicht über Rand hinaus moven
     def clamp(self):
         if self.posY <= 0:
             self.posY = 0
         if self.posY + self.height >= 500:
             self.posY = 500 - self.height
+    #Restart Position und dy
     def restart_pos(self):
         self.posY = 500//2 - 60
         self.draw()
         self.state = "stopped"
+        self.dy = 0
 
 class Collision_Manager:
+
+    #Collision ball paddle2 return True
     def between_ball_and_paddle1(self, ball, paddle1):
         if ball.posY + ball.radius > paddle1.posY and ball.posY - ball.radius < paddle1.posY + paddle1.height:
             if ball.posX - ball.radius <= paddle1.posX + paddle1.width:
                 return True
         return False
+
+    # Collision ball paddle2 return True
     def between_ball_and_paddle2(self, ball, paddle2):
         if ball.posY + ball.radius > paddle2.posY and ball.posY - ball.radius < paddle2.posY + paddle2.height:
             if ball.posX + ball.radius >= paddle2.posX:
                 return True
         return False
 
+    #Ball walls return True
     def between_ball_and_walls(self,ball):
         #top
         if ball.posY -ball.radius <= 0:
@@ -119,9 +139,11 @@ class Collision_Manager:
             return True
         return False
 
+    #Goal1: über x Koordinate x = 852 hinaus return True
     def check_goal_player1(self, ball):
-
         return ball.posX  >= 852
+
+    # Goal2: über x Koordinate x = 48 hinaus return True
     def check_goal_player2(self, ball):
         return ball.posX  <= 48
 
@@ -138,47 +160,54 @@ class Score:
         self.label = self.font.render(self.points, 0, light_grey )
         self.draw()
 
+    #Label zeichnen
     def draw(self):
         self.screen.blit(self.label, (self.posX - self.label.get_rect().width // 2, self.posY))
+
+    #Punkte erhöhen und Label neu rendern
     def increase(self):
         points = int(self.points) + 1
         self.points = str(points)
         self.label = self.font.render(self.points, 0, light_grey)
-    def restart(self):
-        self.points = "0"
-        self.label = self.font.render(self.points, 0, light_grey)
+
+    #Punkte auf null und neu rendern
     def restart(self):
         self.points = "0"
         self.label = self.font.render(self.points, 0, light_grey)
 
 
 class Button:
-    def __init__(self, color, x, y, width, height, text=''):
+    def __init__(self, color, posX, posY, width, height, text='', size = 25):
         self.color = color
-        self.x = x
-        self.y = y
+        self.posX = posX
+        self.posY = posY
         self.width = width
         self.height = height
         self.text = text
+        self.size = size
         self.state = "unvisible"
 
-    def draw(self, win, outline=None):
-        # Call this method to draw the button on the screen
+    #Draw button
+    def draw(self, screen, outline=None):
+        #Wenn Outline größeres Rechteck mit anderer Farbe unten drunter
         if outline:
-            pygame.draw.rect(win, outline, (self.x - 2, self.y - 2, self.width + 4, self.height + 4), 0)
+            pygame.draw.rect(screen, outline, (self.posX - 2, self.posY - 2, self.width + 4, self.height + 4), 0)
 
-        pygame.draw.rect(win, self.color, (self.x, self.y, self.width, self.height), 0)
+        #Rechteck zeichnen
+        pygame.draw.rect(screen, self.color, (self.posX, self.posY, self.width, self.height), 0)
 
+        #Wenn Text nicht leer ist
         if self.text != '':
-            font = pygame.font.SysFont('monospace', 25, bold = True)
+            font = pygame.font.SysFont('monospace', self.size, bold = True)
             text = font.render(self.text, 1, (0, 0, 0))
-            win.blit(text, (
-            self.x + (self.width / 2 - text.get_width() / 2), self.y + (self.height / 2 - text.get_height() / 2)))
+            screen.blit(text, (
+            self.posX + (self.width / 2 - text.get_width() / 2), self.posY + (self.height / 2 - text.get_height() / 2)))
 
+    #Checken ob Maus über Button, wenn ja return True
     def isOver(self, pos):
         # Pos is the mouse position or a tuple of (x,y) coordinates
-        if pos[0] > self.x and pos[0] < self.x + self.width:
-            if pos[1] > self.y and pos[1] < self.y + self.height:
+        if pos[0] > self.posX and pos[0] < self.posX + self.width:
+            if pos[1] > self.posY and pos[1] < self.posY + self.height:
                 return True
 
         return False
--- Ponglocal.py
+++ Ponglocal.py
@@ -1,9 +1,13 @@
 import pygame, sys
 from KLassenlocal import *
 
+#Pygame initialisieren
 pygame.init()
+
+#Frame Rate des Spiels festlegen
 clock = pygame.time.Clock()
 clock.tick(30)
+
 #Variablen
 width = 900
 height = 500
@@ -18,12 +22,12 @@ pygame.display.set_caption("PONG")
 
 
 #Funktionen
+#Paint_back() färbt den Hintergrund des Spielfelds und erstellt die weiße Mittellinie
 def paint_back():
     screen.fill(bg_color)
     pygame.draw.line(screen, white,(width//2, 0), (width//2, height),7 )
 
-
-
+#restart setzt Scores, Paddle positionen sowie den Ball zurück
 def restart():
     paint_back()
     score1.restart()
@@ -32,24 +36,7 @@ def restart():
     paddle1.restart_pos()
     paddle2.restart_pos()
 
-paint_back()
-
-#Objekte
-ball = Ball( screen, white, width//2, height//2, 15 )
-paddle1 = Paddle(screen, light_grey, 15, height//2 - 60, 20,120)
-paddle2 = Paddle(screen, light_grey,width - 20 - 15, height//2 - 60, 20,120)
-collision = Collision_Manager()
-score1 = Score(screen, "0", width//4, 15)
-score2 = Score(screen, "0", width - width//4, 15)
-singleplayer = Button(light_grey, 200, 200, 200, 100, "Single Player" )
-multiplayer = Button(light_grey, 500, 200, 200, 100, "Multi Player" )
-#Variables
-playing = False
-singleplayer.state = "visible"
-multiplayer.state = "visible"
-gamemode = ""
-
-
+#regelt was passiert wenn multiplayer gespielt werden will
 def two():
     paint_back()
     ball.move()
@@ -61,6 +48,8 @@ def two():
     paddle2.clamp()
     paddle2.move()
     paddle2.draw()
+
+#regelt was passiert wenn singleplayer gespielt werden will
 def one():
     paint_back()
     ball.move()
@@ -74,32 +63,65 @@ def one():
     paddle2.move()
     paddle2.draw()
 
+#Objekte werden erzeugt
+ball = Ball( screen, white, width//2, height//2, 15 )
+paddle1 = Paddle(screen, light_grey, 15, height//2 - 60, 20,120)
+paddle2 = Paddle(screen, light_grey,width - 20 - 15, height//2 - 60, 20,120)
+collision = Collision_Manager()
+score1 = Score(screen, "0", width//4, 15)
+score2 = Score(screen, "0", width - width//4, 15)
+singleplayer = Button(light_grey, 200, 200, 200, 100, "Single Player" )
+multiplayer = Button(light_grey, 500, 200, 200, 100, "Multi Player" )
+play = Button(light_grey, 310, 430, 100, 50, "Play", 20 )
+neustarten = Button(light_grey, 490, 430, 100, 50, "Reset", 20 )
+home = Button(light_grey, 10, 10, 70, 50, "Home", 20 )
 
+#Variablen und Parameter von Button Objekten
+playing = False
+singleplayer.state = "visible"
+multiplayer.state = "visible"
+gamemode = ""
+
+#Funltionsaufrufe
+paint_back()
+
+#While Schleife für game
 while True:
     for event in pygame.event.get():
+        #maus position wird jeden Durchlauf in einem Tupel gespeichert
         pos = pygame.mouse.get_pos()
+        #Wenn rotes Kreuz gedrückt wird --> exit
         if event.type == pygame.QUIT:
             sys.exit()
+
         if event.type == pygame.MOUSEBUTTONDOWN:
-            if singleplayer.isOver(pos):
-                gamemode = 0
-                playing = True
-                ball.start_moving()
+            # Wenn Maus auf Multiplayer down Multiplayer Modus
             if multiplayer.isOver(pos):
                 gamemode = 1
                 playing = True
                 ball.start_moving()
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_p:
-                ball.start_moving()
+            #Wenn Maus auf Singleplayer down Singleplayer Modus
+            if singleplayer.isOver(pos):
+                gamemode = 0
                 playing = True
-
-            if event.key == pygame.K_r:
+                ball.start_moving()
+            #Wenn Maus auf Play down Playing = True und ball fängt an zu moven
+            if play.isOver(pos):
+                playing = True
+                ball.start_moving()
+            #Wenn Maus auf neustarten down Punktestand und Pos resetten
+            if neustarten.isOver(pos):
+                restart()
+                playing = False
+            #Wenn Maus auf home down zurück zu Auswahlbildschirm
+            if home.isOver(pos):
                 restart()
                 playing = False
                 singleplayer.state = "visible"
                 multiplayer.state = "visible"
 
+        if event.type == pygame.KEYDOWN:
+            #Steuerung der Paddles mit Keys
             if event.key == pygame.K_UP:
                 paddle2.state = "up"
             if event.key == pygame.K_DOWN:
@@ -108,21 +130,29 @@ while True:
                 paddle1.state = "up"
             if event.key == pygame.K_s:
                 paddle1.state = "down"
+
+        #Auskommentieren wenn man stockende Bewegung der Paddles will wenn man Taste loslässt
         #if event.type == pygame.KEYUP:
             #paddle2.state = "stopped"
             #paddle1.state = "stopped"
 
+    #Variable playing auf False -> es wird nicht gespielt
     if not playing:
         paint_back()
         counter = 0
         counter1 = 0
         counter2 = 0
+        if multiplayer.state == "unvisible" and singleplayer.state == "unvisible":
+            play.draw(screen, light_grey)
+            neustarten.draw(screen, light_grey)
+            home.draw(screen, light_grey)
+        # Knöpfe nur zeigen wenn ihr Parameter auf Visible
         if singleplayer.state == "visible" and multiplayer.state == "visible":
             singleplayer.draw(screen, light_grey)
             multiplayer.draw(screen, light_grey)
             paddle1.draw()
             paddle2.draw()
-
+        #Wenn Knöpfe auf unvisible Spiefeld in Ausgangsposition ohne Knöpfe
         elif multiplayer.state == "unvisible" and singleplayer.state == "unvisible":
             counter1 = 0
             counter2 = 0
@@ -138,57 +168,56 @@ while True:
             ball.draw()
             counter = 0
 
-
+    #Wenn gespielt wird
     if playing:
+        #Knöpfe unsichtbar
         singleplayer.state = "unvisible"
         multiplayer.state = "unvisible"
 
-
+        #Auswahl des Spielmodus
         if gamemode == 0:
             one()
 
-
         if gamemode == 1:
             two()
-            print(paddle1.dy)
-            print(paddle2.dy)
 
-
-
-
+        #Wenn ball und paddle berühren counter hochzählen
         if collision.between_ball_and_paddle1(ball, paddle1):
             ball.paddle_collision(counter, counter1, paddle1)
             counter += 1
             counter1 += 1
+
+        # Wenn ball und paddle berühren counter hochzählen
         if collision.between_ball_and_paddle2(ball, paddle2):
             ball.paddle_collision(counter, counter2, paddle2)
             counter += 1
             counter2 += 1
+
+        # Wenn ball und wand berühren posY reversen
         if collision.between_ball_and_walls(ball):
             ball.wall_collision()
 
+        #Wenn Tor für Player1 Score erhöhen, Positionen resetten und Richtungen wieder auf 0, playing auf False
         if collision.check_goal_player1(ball):
             paint_back()
             score1.increase()
             ball.restart_pos()
             paddle1.restart_pos()
             paddle2.restart_pos()
-            paddle1.dy = 0
-            paddle2.dy = 0
-            #ball.dx = 0
+            play.state = "visible"
+            restart.state = "visible"
             playing = False
+
+        # Wenn Tor für Player2 Score erhöhen, Positionen resetten und Richtungen wieder auf 0, playing auf False
         if collision.check_goal_player2(ball):
             paint_back()
             score2.increase()
             ball.restart_pos()
             paddle1.restart_pos()
             paddle2.restart_pos()
-            paddle1.dy = 0
-            paddle2.dy = 0
-            #ball.dx = 0
             playing = False
 
-
+    #Dauerhaft Score anzeigen und Display updaten
     score1.draw()
     score2.draw()
     pygame.display.update()
